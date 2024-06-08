2e626e2df5971f8a069ec02e24953a247a7327a4
Gustavo <gustavo927318@gmail.com>
2024-06-08 13:48:43 -0300

    perf: optimizing the dark theme and some parts in layout

--- script.js
+++ script.js
@@ -1,66 +1,70 @@
 $(document).ready(function () {
-
-    // Parallax
-    setTimeout(function () {
-        $('header').parallax({ imageSrc: './assets/images/geral/background.webp' });
-        $('.endurance-image').parallax({ imageSrc: './assets/images/geral/endurance.webp' });
-        $('.gargantua-image').parallax({ imageSrc: './assets/images/geral/gargantua.webp' });
-    }, 250);
-
-    // Show or hide header
-    const headerBox = document.querySelector('.header-box-one');
-
-    var prevScrollpos = window.pageYOffset;
-
-    window.onscroll = function () {
-        const currentScrollPos = window.pageYOffset;
-        if (prevScrollpos < currentScrollPos) {
-            headerBox.classList.remove('visible');
-            headerBox.classList.add('hidden');
-        } else if (currentScrollPos === 0) {
-            headerBox.classList.remove('visible');
-            headerBox.classList.remove('hidden');
-        }
-        else {
-            if (prevScrollpos > currentScrollPos) {
-                headerBox.classList.add('visible');
-                headerBox.classList.remove('hidden');
-            }
-        }
-        prevScrollpos = currentScrollPos;
-    };
-
-    // Scroll to sections
-
-    var navBtn = $('.item');
-
-    var home = $('header');
-    var planets = $('#Planets');
-    var characters = $('#Characters');
-    var about = $('#About');
-
-    var scrollTo = '';
-
-    $(navBtn).click(function () {
-
-        var btnId = $(this).attr('id');
-
-        if (btnId == 'home') {
-            scrollTo = home;
-        } else if (btnId == 'planets') {
-            scrollTo = planets;
-        } else if (btnId == 'characters') {
-            scrollTo = characters;
-        } else if (btnId == 'about') {
-            scrollTo = about;
-        } else {
-            scrollTo = home;
-        }
-
-        $([document.documentElement, document.body]).animate({
-            scrollTop: $(scrollTo).offset().top - 70
-        }, 100);
+  // Parallax
+  setTimeout(function () {
+    $("header").parallax({ imageSrc: "./assets/images/geral/background.webp" });
+    $(".endurance-image").parallax({
+      imageSrc: "./assets/images/geral/endurance.webp",
     });
+    $(".gargantua-image").parallax({
+      imageSrc: "./assets/images/geral/gargantua.webp",
+    });
+  }, 250);
+
+  // Show or hide header
+  const headerBox = document.querySelector(".header-box-one");
+
+  var prevScrollpos = window.pageYOffset;
+
+  window.onscroll = function () {
+    const currentScrollPos = window.pageYOffset;
+    if (prevScrollpos < currentScrollPos) {
+      headerBox.classList.remove("visible");
+      headerBox.classList.add("hidden");
+    } else if (currentScrollPos === 0) {
+      headerBox.classList.remove("visible");
+      headerBox.classList.remove("hidden");
+    } else {
+      if (prevScrollpos > currentScrollPos) {
+        headerBox.classList.add("visible");
+        headerBox.classList.remove("hidden");
+      }
+    }
+    prevScrollpos = currentScrollPos;
+  };
+
+  // Scroll to sections
+
+  var navBtn = $(".item");
+
+  var home = $("header");
+  var planets = $("#Planets");
+  var characters = $("#Characters");
+  var about = $("#About");
+
+  var scrollTo = "";
+
+  $(navBtn).click(function () {
+    var btnId = $(this).attr("id");
+
+    if (btnId == "home") {
+      scrollTo = home;
+    } else if (btnId == "planets") {
+      scrollTo = planets;
+    } else if (btnId == "characters") {
+      scrollTo = characters;
+    } else if (btnId == "about") {
+      scrollTo = about;
+    } else {
+      scrollTo = home;
+    }
+
+    $([document.documentElement, document.body]).animate(
+      {
+        scrollTop: $(scrollTo).offset().top - 70,
+      },
+      100
+    );
+  });
 });
 
 // Get full year
@@ -68,180 +72,47 @@ const year = document.getElementById("year");
 year.textContent = new Date().getFullYear();
 
 // Loader
-const loader = document.querySelector('.loader');
-const body = document.querySelector('body');
+const loader = document.querySelector(".loader");
+const body = document.querySelector("body");
 
-window.addEventListener('beforeunload', () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+window.addEventListener("beforeunload", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
 });
 
-window.addEventListener('load', () => {
-    loader.style.display = 'none';
-    body.style.overflow = 'auto';
+window.addEventListener("load", () => {
+  loader.style.display = "none";
+  body.style.overflow = "auto";
 });
 
-window.addEventListener('progress', event => {
-    const { loaded, total } = event;
-    const progress = (loaded / total) * 100;
-    loader.style.display = `${progress}%`;
+window.addEventListener("progress", (event) => {
+  const { loaded, total } = event;
+  const progress = (loaded / total) * 100;
+  loader.style.display = `${progress}%`;
 });
 
-// Dark and light theme
-const toggleThemeButton = document.querySelector('.switch-theme');
+const switchFunction = () => {
+  const rootElement = document.documentElement;
+  let dataTheme = rootElement.getAttribute("data-theme"),
+    newTheme;
 
-//Switch icon
-const switchIcon = document.getElementById('switch-icon');
+  newTheme = dataTheme === "light" ? "dark" : "light";
 
-//Prais
-const praisBox = document.querySelector('.prais-box');
-const praisTitle = document.querySelector('.prais-title');
+  rootElement.setAttribute("data-theme", newTheme);
 
-//Theories
-const theories = document.querySelector('.theories');
+  localStorage.setItem("theme", newTheme);
+};
 
-//Planets
-const planetsCards = document.querySelector('.planets-cards');
-const planetsTitle = document.querySelector('.planets-tittle');
-const cardsConatiner = document.querySelector('.cards-container');
+document
+  .getElementById("theme-switcher")
+  .addEventListener("click", switchFunction);
 
-//Characters
-const charactersContainer = document.querySelector('.characters-container');
+let localS = localStorage.getItem("theme"),
+  themeToSet = localS;
 
-//About
-const about = document.querySelector('.about');
-
-//Footer
-const footer = document.querySelector('footer');
-
-function toggleTheme() {
-    if (body.classList.contains('theme-light')) {
-        //Switch button
-        toggleThemeButton.classList.remove('theme-light');
-        toggleThemeButton.classList.add('theme-dark');
-
-        //Body
-        body.classList.remove('theme-light');
-        body.classList.add('theme-dark');
-
-        //Switch icon
-        switchIcon.classList.remove('fa-moon');
-        switchIcon.classList.add('fa-sun');
-
-        //Prais
-        praisBox.classList.add('theme-dark');
-        praisTitle.classList.add('theme-dark');
-
-        //Theories
-        theories.classList.add('theme-dark');
-
-        //Planets
-        planetsCards.classList.add('theme-dark');
-        planetsTitle.classList.add('theme-dark');
-        cardsConatiner.classList.add('theme-dark');
-
-        //Characters
-        charactersContainer.classList.add('theme-dark');
-
-        //About
-        about.classList.add('theme-dark');
-
-        //Footer
-        footer.classList.add('theme-dark');
-
-        setCookie('theme', 'dark', 365);
-    } else {
-        //Switch button
-        toggleThemeButton.classList.remove('theme-dark');
-        toggleThemeButton.classList.add('theme-light');
-
-        //Body
-        body.classList.remove('theme-dark');
-        body.classList.add('theme-light');
-
-        //Switch icon
-        switchIcon.classList.remove('fa-sun');
-        switchIcon.classList.add('fa-moon');
-
-        //Prais
-        praisBox.classList.remove('theme-dark');
-        praisTitle.classList.remove('theme-dark');
-
-        //Theories
-        theories.classList.remove('theme-dark');
-
-        //Planets
-        planetsCards.classList.remove('theme-dark');
-        planetsTitle.classList.remove('theme-dark');
-        cardsConatiner.classList.remove('theme-dark');
-
-        //Characters
-        charactersContainer.classList.remove('theme-dark');
-
-        //About
-        about.classList.remove('theme-dark');
-
-        //Footer
-        footer.classList.remove('theme-dark');
-
-        setCookie('theme', 'light', 365);
-    }
+if (!localS) {
+  themeToSet = window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light";
 }
 
-toggleThemeButton.addEventListener('click', toggleTheme);
-
-function setCookie(name, value, days) {
-    let expires = '';
-    if (days) {
-        const date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = '; expires=' + date.toUTCString();
-    }
-    document.cookie = name + '=' + (value || '') + expires + '; path=/';
-}
-
-function getCookie(name) {
-    const nameEQ = name + '=';
-    const ca = document.cookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-}
-
-const savedTheme = getCookie('theme');
-
-if (savedTheme) {
-    if (savedTheme === 'dark') {
-        switchIcon.classList.remove('fa-moon');
-        switchIcon.classList.add('fa-sun');
-        toggleThemeButton.classList.add('theme-dark');
-        body.classList.add('theme-dark');
-        praisBox.classList.add('theme-dark');
-        praisTitle.classList.add('theme-dark');
-        theories.classList.add('theme-dark');
-        planetsCards.classList.add('theme-dark');
-        planetsTitle.classList.add('theme-dark');
-        cardsConatiner.classList.add('theme-dark');
-        charactersContainer.classList.add('theme-dark');
-        about.classList.add('theme-dark');
-        footer.classList.add('theme-dark');
-
-    } else {
-        switchIcon.classList.remove('fa-sun');
-        switchIcon.classList.add('fa-moon');
-        toggleThemeButton.classList.remove('theme-dark');
-        switchIcon.classList.add('theme-light');
-        body.classList.add('theme-light');
-        praisBox.classList.remove('theme-dark');
-        praisTitle.classList.remove('theme-dark');
-        theories.classList.remove('theme-dark');
-        planetsCards.classList.remove('theme-dark');
-        planetsTitle.classList.remove('theme-dark');
-        cardsConatiner.classList.remove('theme-dark');
-        charactersContainer.classList.remove('theme-dark');
-        about.classList.remove('theme-dark');
-        footer.classList.remove('theme-dark');
-    }
-}
+document.documentElement.setAttribute("data-theme", themeToSet);
